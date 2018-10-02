6a05f0cb522e2320e4a011eae8d1249ad140c910
Ladislav Smola <lsmola@redhat.com>
2018-10-02 10:59:18 +0200

    Use targeted persistor in specs

--- spec/save_inventory/build_parent_inventory_collections_spec.rb
+++ spec/save_inventory/build_parent_inventory_collections_spec.rb
@@ -1,6 +1,6 @@
 require_relative 'spec_helper'
 require_relative '../helpers/spec_parsed_data'
-require_relative '../persister/test_base_persister'
+require_relative '../persister/test_persister'
 
 describe InventoryRefresh::SaveInventory do
   include SpecHelper
@@ -15,21 +15,21 @@ describe InventoryRefresh::SaveInventory do
                               :network_manager => FactoryGirl.create(:ems_network))
   end
 
-  let(:persister_class) { ::TestBasePersister }
+  let(:persister_class) { ::TestPersister }
   let(:persister) { persister_class.new(@ems, @ems) }
 
   it "checks parent inventory collections defined manually are not overwritten" do
     # Add blank presisters
-    persister.add_collection(:vms_and_templates) do |x|
+    persister.add_collection(persister.cloud, :vms_and_templates) do |x|
       x.add_properties(:model_class => VmOrTemplate)
     end
-    persister.add_collection(:hardwares) do |x|
+    persister.add_collection(persister.cloud, :hardwares) do |x|
       x.add_properties(:manager_ref => %i(vm_or_template), :parent_inventory_collections => %i(vms_and_templates))
     end
-    persister.add_collection(:disks) do |x|
+    persister.add_collection(persister.cloud, :disks) do |x|
       x.add_properties(:manager_ref => %i(hardware device_name), :parent_inventory_collections => %i(vms_and_templates))
     end
-    persister.add_collection(:networks) do |x|
+    persister.add_collection(persister.cloud, :networks) do |x|
       x.add_properties(:manager_ref => %i(hardware description), :parent_inventory_collections => %i(vms_and_templates))
     end
 
@@ -64,10 +64,18 @@ describe InventoryRefresh::SaveInventory do
 
   it "checks parent inventory collections are built correctly based on the model relations" do
     # Add blank presisters
-    persister.add_collection(:vms_and_templates) { |x| x.add_properties(:model_class => VmOrTemplate) }
-    persister.add_collection(:hardwares) { |x| x.add_properties(:manager_ref => %i(vm_or_template)) }
-    persister.add_collection(:disks) { |x| x.add_properties(:manager_ref => %i(hardware device_name)) }
-    persister.add_collection(:networks) { |x| x.add_properties(:manager_ref => %i(hardware description)) }
+    persister.add_collection(persister.cloud, :vms_and_templates) do |x|
+      x.add_properties(:model_class => VmOrTemplate)
+    end
+    persister.add_collection(persister.cloud, :hardwares) do |x|
+      x.add_properties(:manager_ref => %i(vm_or_template), :parent_inventory_collections => nil)
+    end
+    persister.add_collection(persister.cloud, :disks) do |x|
+      x.add_properties(:manager_ref => %i(hardware device_name), :parent_inventory_collections => nil)
+    end
+    persister.add_collection(persister.cloud, :networks) do |x|
+      x.add_properties(:manager_ref => %i(hardware description), :parent_inventory_collections => nil)
+    end
 
     indexed_ics = persister.inventory_collections.index_by(&:name)
     expect(indexed_ics[:vms_and_templates].parent_inventory_collections).to be_nil
@@ -104,16 +112,16 @@ describe InventoryRefresh::SaveInventory do
 
   it "checks parent inventory collections are not overwritten when set as []" do
     # Add blank presisters
-    persister.add_collection(:vms_and_templates) do |x|
+    persister.add_collection(persister.cloud, :vms_and_templates) do |x|
       x.add_properties(:model_class => VmOrTemplate, :parent_inventory_collections => [])
     end
-    persister.add_collection(:hardwares) do |x|
+    persister.add_collection(persister.cloud, :hardwares) do |x|
       x.add_properties(:manager_ref => %i(vm_or_template), :parent_inventory_collections => [])
     end
-    persister.add_collection(:disks) do |x|
+    persister.add_collection(persister.cloud, :disks) do |x|
       x.add_properties(:manager_ref => %i(hardware device_name), :parent_inventory_collections => [])
     end
-    persister.add_collection(:networks) do |x|
+    persister.add_collection(persister.cloud, :networks) do |x|
       x.add_properties(:manager_ref => %i(hardware description), :parent_inventory_collections => [])
     end
 
@@ -132,14 +140,12 @@ describe InventoryRefresh::SaveInventory do
   end
 
   it "checks error is thrown for non-existent inventory collection" do
-    persister.add_collection(:hardwares) do |x|
+    persister.add_collection(persister.cloud, :hardwares) do |x|
       x.add_properties(:manager_ref => %i(vm_or_template), :parent_inventory_collections => %i(random_name))
     end
 
     expect { InventoryRefresh::InventoryCollection::Scanner.scan!(persister.inventory_collections) }.to(
-      raise_exception(
-        "Can't find InventoryCollection :random_name referenced from InventoryCollection:<Hardware>"
-      )
+      raise_error.with_message(/Can't find InventoryCollection :random_name referenced from/)
     )
   end
 end
