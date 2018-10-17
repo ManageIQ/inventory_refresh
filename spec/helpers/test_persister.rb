c4a180981419e5abc1d42b2cd6b44c9fbb943b70
Ladislav Smola <lsmola@redhat.com>
2018-10-17 13:43:11 +0200

    Test data for new retention strategies

--- spec/helpers/test_persister.rb
+++ spec/helpers/test_persister.rb
@@ -2,12 +2,22 @@ require_relative "test_builder"
 require_relative "test_builder/persister_helper"
 
 class TestPersister < InventoryRefresh::Persister
-  attr_reader :manager, :target, :collections
+  attr_reader :manager, :target, :collections, :options
 
   include ::TestBuilder::PersisterHelper
 
   # @return [Config::Options] Options for the manager type
   def options
     @options ||= {}
+  end
+
+  def initialize(manager, target = nil, extra_options = {})
+    @manager = manager
+    @target  = target
+
+    @collections = {}
+    @options     = extra_options
+
+    initialize_inventory_collections
   end
 end
--- spec/persister/targeted_refresh_spec_helper.rb
+++ spec/persister/targeted_refresh_spec_helper.rb
@@ -2,12 +2,12 @@ require_relative '../helpers/test_persister/cloud'
 require_relative '../helpers/test_persister/containers'
 
 module TargetedRefreshSpecHelper
-  def create_persister
-    TestPersister::Cloud.new(@ems, @ems)
+  def create_persister(extra_options = {})
+    TestPersister::Cloud.new(@ems, @ems, extra_options)
   end
 
-  def create_containers_persister
-    TestPersister::Containers.new(@ems, @ems)
+  def create_containers_persister(extra_options = {})
+    TestPersister::Containers.new(@ems, @ems, extra_options)
   end
 
   def expected_ext_management_systems_count
