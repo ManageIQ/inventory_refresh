3241f663d456ba5c5618548f876f99b05323a0d6
Ladislav Smola <lsmola@redhat.com>
2019-04-03 11:53:17 +0200

    Default default and batched saving strategy
    Leaving only :concurrent_safe_batch strategy. Remove only
    specs and helpers that are no longer relevant.

--- spec/factories/authentication.rb
+++
@@ -1,3 +0,0 @@
-FactoryBot.define do
-  factory :auth_key_pair_cloud, :class => "ManageIQ::Providers::CloudManager::AuthKeyPair"
-end
--- spec/models/auth_key_pair.rb
+++
@@ -1,4 +0,0 @@
-require "authentication"
-
-class AuthKeyPair < Authentication
-end
